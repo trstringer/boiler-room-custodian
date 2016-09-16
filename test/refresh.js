666096060e3593e10c482800b95a2cbc32e953d3
Thomas Stringer <tr.stringer@outlook.com>
2016-09-16 15:16:45 -0400

    fix test util bug

--- test/refresh.js
+++ test/refresh.js
@@ -1,38 +1,48 @@
 const fs = require('fs');
 
-function copyFile(source, destination, callback) {
-  console.log('helloooooo');
-  fs.readFile(source, 'utf8', (err, data) => {
-    console.log(`read file ${source}`);
-    if (err) {
-      callback(err);
-    }
-    else {
-      console.log(`writing file ${destination}`);
-      fs.writeFile(destination, data, callback);
-    }
+function copyFile(source, destination) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(source, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        fs.writeFile(destination, data, (err) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve();
+          }
+        });
+      }
+    });
+  });
+}
+
+function removeFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.unlink(file, (err) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve();
+      }
+    });
   });
 }
 
 module.exports = (done) => {
-  // fs.createReadStream(`${__dirname}/repo/test-file1.save.txt`)
-  //   .pipe(fs.createWriteStream(`${__dirname}/repo/test-file1.txt`));
-
-  console.log('doing refresh');
-  copyFile(`${__dirname}/repo/test-file1.save.txt`, `${__dirname}/repo/test-file1.txt`, (err) => {
-    if (err) {
-      console.log(err.message);
-    }
-
-    done();
-  });
-
-  // fs.createReadStream(`${__dirname}/repo/test-file2.save.txt`)
-  //   .pipe(fs.createWriteStream(`${__dirname}/repo/test-file2.txt`));
-
-  // fs.createReadStream(`${__dirname}/repo/test-file3.save.txt`)
-  //   .pipe(fs.createWriteStream(`${__dirname}/repo/test-file3.txt`));
-
-  fs.unlink(`${__dirname}/repo/new-add-file1.txt`);
-  fs.unlink(`${__dirname}/repo/new-add-file2.txt`);
+  Promise.all([
+    copyFile(`${__dirname}/repo/test-file1.save.txt`, `${__dirname}/repo/test-file1.txt`),
+    copyFile(`${__dirname}/repo/test-file2.save.txt`, `${__dirname}/repo/test-file2.txt`),
+    copyFile(`${__dirname}/repo/test-file3.save.txt`, `${__dirname}/repo/test-file3.txt`),
+    removeFile(`${__dirname}/repo/new-add-file1.txt`),
+    removeFile(`${__dirname}/repo/new-add-file2.txt`)
+  ])
+    .then(() => done())
+    .catch((err) => {
+      throw err;
+    });
 };
