cf95a23d2e61267185d9e46a99737ad1ff778b9e
Thomas Stringer <tr.stringer@outlook.com>
2017-04-07 14:15:58 -0400

    add parse file test

--- tests/fileops.test.js
+++ tests/fileops.test.js
@@ -37,3 +37,36 @@ test('file is deleted', done => {
       done();
     });
 });
+
+test('file is parsed', done => {
+  const pathToFile = path.join(
+    __dirname,
+    'test-files/file1.txt'
+  );
+  // the line counts before and after are
+  // at the moment hard coded, as there is
+  // an obvious chicken-and-egg issue to dynamically
+  // generate what the line count should be afterward
+  //
+  // at this point, the development effort
+  // is not worth it and the test file should
+  // be generally speaking constant. plus, this
+  // is what tests are for
+  const lineCountBefore = 27;
+  const lineCountToRemove = 14;
+  const lineCountAfter = lineCountBefore - lineCountToRemove;
+
+  fileops.readFile(pathToFile)
+    .then(() => fileops.readFile(pathToFile))
+    .then(data => expect(data.split('\n').length).toBe(lineCountBefore))
+    .then(() => fileops.parseFile(pathToFile))
+    .then(() => fileops.readFile(pathToFile))
+    .then(data => {
+      expect(data.split('\n').length).toBe(lineCountAfter);
+      done();
+    })
+    .catch(err => {
+      expect(err).toBeFalsy();
+      done();
+    });
+});
