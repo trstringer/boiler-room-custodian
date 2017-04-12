cb84b39147bb6b2b26ba7f005ea8d1ab0a6fdc1f
Thomas Stringer <tr.stringer@outlook.com>
2017-04-12 11:15:59 -0400

    refactor range delete to not use streams

--- app/modules/file-ops.js
+++ app/modules/file-ops.js
@@ -37,54 +37,50 @@ module.exports.deleteLine = function (file, pattern, callback) {
 };
 
 module.exports.deleteRange = function (file, range, callback) {
-  const source = readline.createInterface({
-    input: fs.createReadStream(file)
-  });
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
 
-  const tempFileName = `${file}.tmp`;
-  const tempFileStream = fs.createWriteStream(tempFileName);
+    const lines = data.split('\n');
 
-  // if the range is a single digit value (i.e. '4') then just delete
-  // the single line. Otherwise if it is a range in the format of
-  // 'start,end' then remove from this line range inclusively
-  let startLineNumber, endLineNumber;
-  const singleLine = Number(range);
-  if (singleLine) {
-    startLineNumber = singleLine;
-    endLineNumber = singleLine;
-  }
-  else {
-    const rangeMatch = /(\d+),(\d+)/.exec(range);
-    startLineNumber = Number(rangeMatch[1]);
-    endLineNumber = Number(rangeMatch[2]);
-    if (!startLineNumber || !endLineNumber) {
-      callback(Error('Incorrect line range format'));
+    // if the range is a single digit value (i.e. '4') then just delete
+    // the single line. Otherwise if it is a range in the format of
+    // 'start,end' then remove from this line range inclusively
+    let startLineNumber, endLineNumber;
+    const singleLine = Number(range);
+    if (singleLine) {
+      startLineNumber = singleLine;
+      endLineNumber = singleLine;
     }
     else {
-      let lineNumber = 1;
-      source.on('line', (line) => {
-        if (lineNumber < startLineNumber || lineNumber > endLineNumber) {
-          if (lineNumber > 1) {
-            tempFileStream.write('\n');
-          }
-          tempFileStream.write(`${line}`);
-        }
-        lineNumber++;
-      });
-      source.on('close', () => {
-        tempFileStream.close();
-        // delete the old file and rename the temp file
-        fs.unlink(file, (err) => {
-          if (err) {
-            callback(err);
-          }
-          else {
-            fs.rename(tempFileName, file, callback);
-          }
-        });
-      });
+      const rangeMatch = /(\d+),(\d+)/.exec(range);
+      startLineNumber = Number(rangeMatch[1]);
+      endLineNumber = Number(rangeMatch[2]);
+      if (!startLineNumber || !endLineNumber) {
+        callback(Error('Incorrect line range format'));
+      }
     }
-  }
+
+    let tempOutput = '';
+
+    let i = 0;
+    let max = lines.length;
+    for (i = 0; i < max; i++) {
+      if (i + 1 >= startLineNumber && i + 1 <= endLineNumber) {
+        continue;
+      }
+      if (tempOutput === '') {
+        tempOutput = lines[i];
+      }
+      else {
+        tempOutput += `\n${lines[i]}`;
+      }
+    }
+
+    fs.writeFile(file, tempOutput, 'utf8', callback);
+  });
 };
 
 module.exports.replace = function (file, replaceList, callback) {
